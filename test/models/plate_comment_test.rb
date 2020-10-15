6609db3bfddd477b4c0ce219f6fbf8d8c342e5fd
Francisca Hohlberg <fhohlberg@uc.cl>
2020-10-15 00:29:21 -0300

    vendor compiled assets

--- test/models/plate_comment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PlateCommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/restaurant_comment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class RestaurantCommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
